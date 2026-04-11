7e4efaf8f1bb0bfc53c78661986f73583bfa1df3
Võ Minh Nhựt <bnhut465@gmail.com>
2026-04-11 19:35:12 +0700

    Cấu hình Firebase SDK và thêm file serviceAccountKey.json

---
+++ src/main/java/com/minesweeper/util/FirebaseUtil.java
@@ -0,0 +1,11 @@
+package com.minesweeper.util;
+
+/**
+ * Skeleton for FirebaseUtil
+ * UC feature: Firebase connection setup
+ */
+public class FirebaseUtil {
+    // TODO: Stub methods for developer review
+}
+
+// Commit: Cấu hình Firebase SDK và thêm file serviceAccountKey.json | Author: Võ Minh Nhựt | Date: 2026-04-11 19:35:12
---
+++ src/main/webapp/js/firebase-init.js
@@ -0,0 +1,8 @@
+/**
+ * JS Skeleton: Firebase SDK initialization JS
+ */
+(function() {
+    console.log('Firebase SDK initialization JS client initialized.');
+})();
+
+// Commit: Cấu hình Firebase SDK và thêm file serviceAccountKey.json | Author: Võ Minh Nhựt | Date: 2026-04-11 19:35:12
