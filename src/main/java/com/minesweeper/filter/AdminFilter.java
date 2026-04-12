13b8954c728046c3cb6207f68e2726da6b68ab1e
Nguyễn Duy Khánh <Ntnq3698@gmail.com>
2026-04-12 10:15:30 +0700

    Thêm skeleton cho các Filter bảo mật (AuthFilter, AdminFilter)

---
+++ src/main/java/com/minesweeper/filter/AdminFilter.java
@@ -0,0 +1,11 @@
+package com.minesweeper.filter;
+
+/**
+ * Skeleton for AdminFilter
+ * UC feature: Admin filtering
+ */
+public class AdminFilter {
+    // TODO: Stub methods for developer review
+}
+
+// Commit: Thêm skeleton cho các Filter bảo mật (AuthFilter, AdminFilter) | Author: Nguyễn Duy Khánh | Date: 2026-04-12 10:15:30
---
+++ src/main/java/com/minesweeper/filter/AuthFilter.java
@@ -0,0 +1,11 @@
+package com.minesweeper.filter;
+
+/**
+ * Skeleton for AuthFilter
+ * UC feature: UC-4 & Admin authorization filtering
+ */
+public class AuthFilter {
+    // TODO: Stub methods for developer review
+}
+
+// Commit: Thêm skeleton cho các Filter bảo mật (AuthFilter, AdminFilter) | Author: Nguyễn Duy Khánh | Date: 2026-04-12 10:15:30
