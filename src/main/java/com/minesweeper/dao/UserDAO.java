09540260f7ca4d47911d197ae4a8b9160c7ed145
Võ Minh Nhựt <bnhut465@gmail.com>
2026-04-16 19:40:12 +0700

    Tạo model User và interface UserDAO để truy xuất thông tin tài khoản

---
+++ src/main/java/com/minesweeper/dao/UserDAO.java
@@ -0,0 +1,9 @@
+package com.minesweeper.dao;
+
+/**
+ * Skeleton for UserDAO
+ * UC feature: User Firestore DAO CRUD stubs
+ */
+public class UserDAO {
+    // TODO: Stub methods for developer review
+}
---
+++ src/main/java/com/minesweeper/model/User.java
@@ -0,0 +1,9 @@
+package com.minesweeper.model;
+
+/**
+ * Skeleton for User
+ * UC feature: User details model
+ */
+public class User {
+    // TODO: Stub methods for developer review
+}
