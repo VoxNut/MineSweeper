6b3aa3982ade880d6599f0e87d3d8a285598caca
Võ Minh Nhựt <bnhut465@gmail.com>
2026-04-16 19:40:12 +0700

    Tạo model User và interface UserDAO để truy xuất thông tin tài khoản

---
+++ src/main/java/com/minesweeper/model/User.java
@@ -0,0 +1,11 @@
+package com.minesweeper.model;
+
+/**
+ * Skeleton for User
+ * UC feature: User details model
+ */
+public class User {
+    // TODO: Stub methods for developer review
+}
+
+// Commit: Tạo model User và interface UserDAO để truy xuất thông tin tài khoản | Author: Võ Minh Nhựt | Date: 2026-04-16 19:40:12
