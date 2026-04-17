1915b346b0fa0a8659a876c46a7dc46a79ed6545
Võ Minh Nhựt <bnhut465@gmail.com>
2026-04-17 21:50:33 +0700

    Thêm logic đăng nhập email/password trong AuthService

---
+++ src/main/java/com/minesweeper/service/AuthService.java
@@ -0,0 +1,9 @@
+package com.minesweeper.service;
+
+/**
+ * Skeleton for AuthService
+ * UC feature: Authentication logic
+ */
+public class AuthService {
+    // TODO: Stub methods for developer review
+}
