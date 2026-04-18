29d750375e5c7404a2a5e02dd5edd56658ec3911
Võ Minh Nhựt <bnhut465@gmail.com>
2026-04-18 09:30:00 +0700

    Xử lý đăng nhập trong LoginServlet và kết nối file JS xử lý sự kiện

---
+++ src/main/java/com/minesweeper/servlet/auth/LoginServlet.java
@@ -0,0 +1,11 @@
+package com.minesweeper.servlet;
+
+/**
+ * Skeleton for LoginServlet
+ * UC feature: UC-2: Login handling
+ */
+public class LoginServlet {
+    // TODO: Stub methods for developer review
+}
+
+// Commit: Xử lý đăng nhập trong LoginServlet và kết nối file JS xử lý sự kiện | Author: Võ Minh Nhựt | Date: 2026-04-18 09:30:00
---
+++ src/main/webapp/js/auth.js
@@ -0,0 +1,8 @@
+/**
+ * JS Skeleton: Auth client listener
+ */
+(function() {
+    console.log('Auth client listener client initialized.');
+})();
+
+// Commit: Xử lý đăng nhập trong LoginServlet và kết nối file JS xử lý sự kiện | Author: Võ Minh Nhựt | Date: 2026-04-18 09:30:00
