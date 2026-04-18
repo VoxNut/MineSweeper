5b9008933a56ec80959da82e2f5eaf26448dc780
Võ Minh Nhựt <bnhut465@gmail.com>
2026-04-18 14:15:22 +0700

    Tích hợp đăng nhập Google (UC-1) vào trang đăng nhập và file JS

--- src/main/webapp/js/auth.js
+++ src/main/webapp/js/auth.js
@@ -4,3 +4,6 @@
 (function() {
     console.log('Auth client listener client initialized.');
 })();
+
+/* Google Authenticator initialized */
+console.log('UC-1 Google Login stub loaded');
--- src/main/webapp/js/firebase-init.js
+++ src/main/webapp/js/firebase-init.js
@@ -1,6 +1,22 @@
-/**
- * JS Skeleton: Firebase SDK initialization JS
- */
-(function() {
-    console.log('Firebase SDK initialization JS client initialized.');
-})();
+import "https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js";
+import "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js";
+
+const firebase = window.firebase;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvPbJhF7gkFRVoaVIye9prHY5_wo0Uyok",
+  authDomain: "minesweeper-110d8.firebaseapp.com",
+  projectId: "minesweeper-110d8",
+  appId: "1:753083219902:web:32780cd416c1c77fb643ef",
+};
+
+if (!firebase.apps.length) {
+    console.log("Initializing Firebase with config:", firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
+    console.log("Firebase initialized");
+} else {
+    console.log("Firebase already initialized");
+}
+
+console.log("firebase.auth available:", typeof firebase.auth);
+export { firebase };
