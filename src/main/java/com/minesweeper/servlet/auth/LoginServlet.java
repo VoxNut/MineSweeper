c1ed2ab5e64767f09300247d94fea52e0877a77e
Võ Minh Nhựt <bnhut465@gmail.com>
2026-04-18 09:30:00 +0700

    Xử lý đăng nhập trong LoginServlet và kết nối file JS xử lý sự kiện

---
+++ src/main/java/com/minesweeper/servlet/auth/LoginServlet.java
@@ -0,0 +1,9 @@
+package com.minesweeper.servlet;
+
+/**
+ * Skeleton for LoginServlet
+ * UC feature: UC-2: Login handling
+ */
+public class LoginServlet {
+    // TODO: Stub methods for developer review
+}
---
+++ src/main/webapp/js/auth.js
@@ -0,0 +1,6 @@
+/**
+ * JS Skeleton: Auth client listener
+ */
+(function() {
+    console.log('Auth client listener client initialized.');
+})();
