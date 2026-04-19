ad3bf370427d920cd9e22f48bef58d55b7d8037d
Nguyễn Duy Khánh <Ntnq3698@gmail.com>
2026-04-19 15:40:30 +0700

    Thêm xử lý gửi email khôi phục mật khẩu trong auth.js

--- src/main/webapp/js/auth.js
+++ src/main/webapp/js/auth.js
@@ -11,3 +11,7 @@
 console.log('UC-1 Google Login stub loaded');
 
 // Commit: Tích hợp đăng nhập Google (UC-1) vào trang đăng nhập và file JS | Author: Võ Minh Nhựt | Date: 2026-04-18 14:15:22
+
+/* Forgot password handling auth triggers */
+
+// Commit: Thêm xử lý gửi email khôi phục mật khẩu trong auth.js | Author: Nguyễn Duy Khánh | Date: 2026-04-19 15:40:30
