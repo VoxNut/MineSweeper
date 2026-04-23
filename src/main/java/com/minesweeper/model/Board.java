683c630547e9ff829ce90e94bb2a230dfe2e1e90
Nguyễn Đức Khải <23130142@st.hcmuaf.edu.vn>
2026-04-23 20:30:15 +0700

    Thiết kế model Cell và Board cho logic Minesweeper (UC-8)

---
+++ src/main/java/com/minesweeper/model/Board.java
@@ -0,0 +1,9 @@
+package com.minesweeper.model;
+
+/**
+ * Skeleton for Board
+ * UC feature: Board coordinate matrices
+ */
+public class Board {
+    // TODO: Stub methods for developer review
+}
---
+++ src/main/java/com/minesweeper/model/Cell.java
@@ -0,0 +1,9 @@
+package com.minesweeper.model;
+
+/**
+ * Skeleton for Cell
+ * UC feature: Game Grid Cell definitions
+ */
+public class Cell {
+    // TODO: Stub methods for developer review
+}
