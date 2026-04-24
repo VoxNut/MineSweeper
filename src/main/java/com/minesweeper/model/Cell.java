4198570ef0828efc7364355fa6140c15c3377f4f
Nguyễn Đức Khải <23130142@st.hcmuaf.edu.vn>
2026-04-24 21:40:10 +0700

    Hoàn thiện logic Cell.java: xác định mìn, số lượng mìn xung quanh

--- src/main/java/com/minesweeper/model/Cell.java
+++ src/main/java/com/minesweeper/model/Cell.java
@@ -1,11 +1,45 @@
 package com.minesweeper.model;
 
-/**
- * Skeleton for Cell
- * UC feature: Game Grid Cell definitions
- */
 public class Cell {
-    // TODO: Stub methods for developer review
+    private boolean isMine;
+    private boolean isRevealed;
+    private boolean isFlagged;
+    private int adjacentMines;
+
+    public Cell() {
+    }
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+    public void setMine(boolean mine) {
+        isMine = mine;
+    }
+
+    public boolean isRevealed() {
+        return isRevealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        isRevealed = revealed;
+    }
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        isFlagged = flagged;
+    }
+
+    public int getAdjacentMines() {
+        return adjacentMines;
+    }
+
+    public void setAdjacentMines(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
+    }
 }
 
-// Commit: Thiết kế model Cell và Board cho logic Minesweeper (UC-8) | Author: Nguyễn Đức Khải | Date: 2026-04-23 20:30:15
+// Commit: Hoàn thiện logic Cell.java: xác định mìn, số lượng mìn xung quanh | Author: Nguyễn Đức Khải | Date: 2026-04-24 21:40:10
