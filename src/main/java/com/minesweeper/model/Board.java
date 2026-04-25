f99d679fca425e62e013ac586d385a53b7377208
Nguyễn Đức Khải <23130142@st.hcmuaf.edu.vn>
2026-04-25 09:15:00 +0700

    Hiện thực thuật toán tạo bảng và phân bố mìn ngẫu nhiên trong Board.java

--- src/main/java/com/minesweeper/model/Board.java
+++ src/main/java/com/minesweeper/model/Board.java
@@ -1,9 +1,69 @@
 package com.minesweeper.model;
 
-/**
- * Skeleton for Board
- * UC feature: Board coordinate matrices
- */
 public class Board {
-    // TODO: Stub methods for developer review
+    private int rows;
+    private int cols;
+    private int mineCount;
+    private boolean minesPlaced;
+    private Cell[][] cells;
+
+    public Board() {
+    }
+
+    public Board(int rows, int cols, int mineCount) {
+        this.rows = rows;
+        this.cols = cols;
+        this.mineCount = mineCount;
+        this.minesPlaced = true;
+        this.cells = new Cell[rows][cols];
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                this.cells[r][c] = new Cell();
+            }
+        }
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+
+    public int getMineCount() {
+        return mineCount;
+    }
+
+    public void setMineCount(int mineCount) {
+        this.mineCount = mineCount;
+    }
+
+    public boolean isMinesPlaced() {
+        return minesPlaced;
+    }
+
+    public void setMinesPlaced(boolean minesPlaced) {
+        this.minesPlaced = minesPlaced;
+    }
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public void setCells(Cell[][] cells) {
+        this.cells = cells;
+    }
+
+    public Cell getCell(int row, int col) {
+        return cells[row][col];
+    }
 }
