ec56ecf09b5d1f045b1315a41e71373158b999f1
Nguyễn Đức Khải <23130142@st.hcmuaf.edu.vn>
2026-04-25 15:30:45 +0700

    Tạo GameService để quản lý trạng thái trò chơi (UC-8)

---
+++ src/main/java/com/minesweeper/service/GameService.java
@@ -0,0 +1,11 @@
+package com.minesweeper.service;
+
+/**
+ * Skeleton for GameService
+ * UC feature: Game core orchestrator service
+ */
+public class GameService {
+    // TODO: Stub methods for developer review
+}
+
+// Commit: Tạo GameService để quản lý trạng thái trò chơi (UC-8) | Author: Nguyễn Đức Khải | Date: 2026-04-25 15:30:45
