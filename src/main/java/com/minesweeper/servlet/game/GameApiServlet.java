625959e9502798a9a2f54d639ec602e68de1ce79
Nguyễn Đức Khải <23130142@st.hcmuaf.edu.vn>
2026-04-26 10:45:10 +0700

    Viết GameApiServlet để xử lý các request chơi game qua API

---
+++ src/main/java/com/minesweeper/servlet/game/GameApiServlet.java
@@ -0,0 +1,9 @@
+package com.minesweeper.servlet;
+
+/**
+ * Skeleton for GameApiServlet
+ * UC feature: Game play REST actions handlers
+ */
+public class GameApiServlet {
+    // TODO: Stub methods for developer review
+}
