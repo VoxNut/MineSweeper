6b6592bd1ef2752d7603bb1c8760cfe4bdbcce3a
Nguyễn Đức Khải <23130142@st.hcmuaf.edu.vn>
2026-04-26 16:20:00 +0700

    Viết game.js để vẽ bảng game và bắt các sự kiện click chuột

---
+++ src/main/webapp/js/game.js
@@ -0,0 +1,8 @@
+/**
+ * JS Skeleton: Event loop for cells click
+ */
+(function() {
+    console.log('Event loop for cells click client initialized.');
+})();
+
+// Commit: Viết game.js để vẽ bảng game và bắt các sự kiện click chuột | Author: Nguyễn Đức Khải | Date: 2026-04-26 16:20:00
