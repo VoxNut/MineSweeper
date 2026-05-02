6e5387edfda7bf11950c6958c70d7c13a1e1a977
Tạ Văn Huy <huyvan2705@gmail.com>
2026-05-02 21:05:45 +0700

    Tạo ScoreService để điều phối lưu điểm và lịch sử chơi

---
+++ src/main/java/com/minesweeper/service/ScoreService.java
@@ -0,0 +1,9 @@
+package com.minesweeper.service;
+
+/**
+ * Skeleton for ScoreService
+ * UC feature: Score statistics logic business handles
+ */
+public class ScoreService {
+    // TODO: Stub methods for developer review
+}
