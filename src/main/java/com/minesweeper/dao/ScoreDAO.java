388466a8f00ce8b6a83a5d0e4cdbfef118545336
Tạ Văn Huy <huyvan2705@gmail.com>
2026-05-02 15:35:12 +0700

    Thiết kế model Score và ScoreDAO để lưu kết quả chơi (UC-14)

---
+++ src/main/java/com/minesweeper/dao/ScoreDAO.java
@@ -0,0 +1,11 @@
+package com.minesweeper.dao;
+
+/**
+ * Skeleton for ScoreDAO
+ * UC feature: Score metrics read write CRUD Firestore stubs
+ */
+public class ScoreDAO {
+    // TODO: Stub methods for developer review
+}
+
+// Commit: Thiết kế model Score và ScoreDAO để lưu kết quả chơi (UC-14) | Author: Tạ Văn Huy | Date: 2026-05-02 15:35:12
---
+++ src/main/java/com/minesweeper/model/Score.java
@@ -0,0 +1,11 @@
+package com.minesweeper.model;
+
+/**
+ * Skeleton for Score
+ * UC feature: Score details schema model
+ */
+public class Score {
+    // TODO: Stub methods for developer review
+}
+
+// Commit: Thiết kế model Score và ScoreDAO để lưu kết quả chơi (UC-14) | Author: Tạ Văn Huy | Date: 2026-05-02 15:35:12
