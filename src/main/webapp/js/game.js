70056d2d067c5122406c8a23db36d665c1112fb2
Nguyễn Đức Khải <23130142@st.hcmuaf.edu.vn>
2026-05-02 10:10:00 +0700

    Sửa lỗi không vẽ lại bảng khi click vào ô đã mở trong game.js

--- src/main/webapp/js/game.js
+++ src/main/webapp/js/game.js
@@ -10,3 +10,7 @@
 /* Open cells click events */
 
 // Commit: Xử lý sự kiện click mở ô và cắm cờ trong game.js | Author: Nguyễn Đức Khải | Date: 2026-04-30 14:15:20
+
+/* Fix click issues on revealed cells */
+
+// Commit: Sửa lỗi không vẽ lại bảng khi click vào ô đã mở trong game.js | Author: Nguyễn Đức Khải | Date: 2026-05-02 10:10:00
