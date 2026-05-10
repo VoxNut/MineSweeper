b0df4af8909bf9024c7a581e65682cd5ec0b7a5d
Võ Minh Nhựt <bnhut465@gmail.com>
2026-05-10 15:40:00 +0700

    Viết AdminLoginServlet xử lý xác thực tài khoản quản trị

---
+++ src/main/java/com/minesweeper/servlet/auth/AdminLoginServlet.java
@@ -0,0 +1,15 @@
+package com.minesweeper.servlet.auth;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminLoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/admin/login.jsp").forward(request, response);
+    }
+}
