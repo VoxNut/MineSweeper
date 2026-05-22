7fcf6b99f15999a8ca31d8e199aeecda9df0a9e8
Nguyễn Duy Khánh <Ntnq3698@gmail.com>
2026-05-22 22:10:00 +0700

    Hoàn thiện AdminFilter ngăn chặn truy cập trái phép trang quản trị

--- src/main/java/com/minesweeper/filter/AdminFilter.java
+++ src/main/java/com/minesweeper/filter/AdminFilter.java
@@ -1,11 +1,46 @@
 package com.minesweeper.filter;
 
-/**
- * Skeleton for AdminFilter
- * UC feature: Admin filtering
- */
-public class AdminFilter {
-    // TODO: Stub methods for developer review
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AdminFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
+
+        if (path.equals("/admin/login")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        HttpSession session = httpRequest.getSession(false);
+
+        if (session != null && "admin".equals(session.getAttribute("role"))) {
+            chain.doFilter(request, response);
+            return;
+        }
+        httpResponse.sendRedirect(httpRequest.getContextPath() + "/home");
+    }
+
+    @Override
+    public void destroy() {
+    }
 }
 
-// Commit: Thêm skeleton cho các Filter bảo mật (AuthFilter, AdminFilter) | Author: Nguyễn Duy Khánh | Date: 2026-04-12 10:15:30
+// Commit: Hoàn thiện AdminFilter ngăn chặn truy cập trái phép trang quản trị | Author: Nguyễn Duy Khánh | Date: 2026-05-22 22:10:00
