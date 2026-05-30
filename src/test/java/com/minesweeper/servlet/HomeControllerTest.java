ee745e7bddb9359aefc86cc86d08d64d4fc5c594
Võ Minh Nhựt <bnhut465@gmail.com>
2026-05-30 21:05:00 +0700

    Viết thêm các unit test ban đầu cho HomeController

---
+++ src/test/java/com/minesweeper/servlet/HomeControllerTest.java
@@ -0,0 +1,40 @@
+package com.minesweeper.servlet;
+
+import com.minesweeper.servlet.HomeServlet;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class HomeControllerTest {
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private RequestDispatcher dispatcher;
+
+    @InjectMocks
+    private HomeServlet homeServlet;
+
+    @Test
+    public void testDoGet() throws Exception {
+        when(request.getRequestDispatcher("/WEB-INF/views/home.jsp")).thenReturn(dispatcher);
+        
+        homeServlet.doGet(request, response);
+        
+        verify(request, times(1)).getRequestDispatcher("/WEB-INF/views/home.jsp");
+        verify(dispatcher, times(1)).forward(request, response);
+    }
+}
+
+// Commit: Viết thêm các unit test ban đầu cho HomeController | Author: Võ Minh Nhựt | Date: 2026-05-30 21:05:00
